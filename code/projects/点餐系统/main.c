6019115f71b3579a1843d1a249d93f2dce0fd39b
肖扬 <3413851324@qq.com>
2026-02-07 17:17:26 +0800

    main.c

--- code/projects/点餐系统/main.c
+++ code/projects/点餐系统/main.c
@@ -1,1 +1,38 @@
+#include "premise.h"
+char Merchant_password[20] = "123456";
+int main() {
+	int choice=-1;
+	Store stores[Max_store];
+	int store_count=0;
+	init_store(stores, &store_count);
+	User users[Max_table];
+	int user_count = 0;
+	init_user(users, &user_count, store_count, stores);
+	Vip vips[100];
+	int vip_count = 0;
+	load_vip(vips, &vip_count);
+	while (1) {
+		printf("\n=======欢迎光临‘沪上阿姨’=======\n");
+		printf("选择您想要的服务\n");
+		printf("1.用户\n");
+		printf("2.商家\n");
+		printf("0.退出程序\n");
+		printf("请选择功能：");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:user_system(stores, store_count, &user_count, vips, &vip_count);
+			break;
+		case 2:
+			merchant_system(stores,&store_count,users,&user_count);
+			break;
+		case 0:
+			exit(0);
 
+		default:
+			printf("无效输入！请重新输入！");
+			break;
+		}
+	}
+	return 0;
+}
